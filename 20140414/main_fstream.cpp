d0d461c40660ea17af8f0b6a025136ffbc37979e
jklim <gocobe1228@naver.com>
2014-04-14 19:51:09 +0900

    @Win32API solution Added.
    ...

---
+++ 20140414/main_fstream.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+struct ItemInfo
+{
+	int price;
+	char name[20];
+
+	friend ostream& operator << (ostream& os, const ItemInfo& obj)
+	{
+		os << obj.name << ", " << obj.price << endl;
+		return os;
+	}
+};
+
+int main(void)
+{
+	fstream file;
+
+	file.open("test.txt", ios_base::out | ios_base::binary);
+
+	ItemInfo a;
+	a.price = 10;
+	strcpy_s(a.name, 20, "hello");
+
+	//file << a << endl;
+
+	file.write((const char*)&a, sizeof(ItemInfo));
+
+	file.close();
+
+	file.open("test.txt", ios_base::in | ios_base::binary);
+
+	ItemInfo b;
+
+	file.read((char*)&b, sizeof(ItemInfo));
+
+	file.close();
+
+	cout << b.name << ", " << b.price << endl;
+
+	_getch();
+	return 0;
+}
---
+++ Win32API/20140414/winmain.cpp
@@ -0,0 +1,73 @@
+﻿#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND,UINT,WPARAM,LPARAM);
+
+INT WINAPI WinMain(HINSTANCE hInst,
+				   HINSTANCE hPrevInst,
+				   LPSTR lpCmdLine,
+				   INT nShowCmd)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_VREDRAW | CS_HREDRAW;
+    wcex.lpfnWndProc = &WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInst;
+	wcex.hIcon = ::LoadIcon(NULL, IDI_APPLICATION);
+	wcex.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)::GetStockObject(WHITE_BRUSH);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = "MainWindowClass";
+    wcex.hIconSm = ::LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!::RegisterClassEx(&wcex))
+	{
+		DWORD dwError = ::GetLastError();
+		return 1;
+	}
+
+	HWND hWnd = ::CreateWindowEx(0, 
+				"MainWindowClass", 
+				"Win32 Sample",
+				WS_OVERLAPPEDWINDOW, 
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				CW_USEDEFAULT,
+				NULL,
+				NULL,
+				hInst,
+				NULL);
+
+	if (hWnd == NULL)
+	{
+		DWORD dwError = ::GetLastError();
+		return 2;
+	}
+
+	::ShowWindow(hWnd, nShowCmd);
+	::UpdateWindow(hWnd);
+
+	MSG msg;
+
+	while (::GetMessage(&msg, NULL, 0, 0))
+	{
+		::TranslateMessage(&msg);
+		::DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
+{
+	if (uMsg == WM_DESTROY)
+	{
+		::PostQuitMessage(0);
+		return 0;
+	}
+
+	return ::DefWindowProc(hWnd,uMsg,wParam,lParam);
+}
