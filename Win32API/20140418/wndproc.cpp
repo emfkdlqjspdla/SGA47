00fcd7433491a6b55e7afd1914b25549edee0152
jklim <gocobe1228@naver.com>
2014-04-18 18:52:02 +0900

    @0418 수정
    ..

--- Win32API/20140418/wndproc.cpp
+++ Win32API/20140418/wndproc.cpp
@@ -16,10 +16,23 @@ LRESULT CALLBACK WndProc(HWND hWnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
 		HPEN hPen = ::CreatePen(PS_SOLID, 1, RGB(255,0,0));
 		HPEN hOldPen = (HPEN)::SelectObject(hdc, hPen);
 
+		HBRUSH hBrush = ::CreateSolidBrush(RGB(0,0,255));
+		HBRUSH hOldBrush = (HBRUSH)::SelectObject(hdc, hBrush);
+
 		POINT pt;
 
-		::MoveToEx(hdc, 0, 0, &pt);
+		::MoveToEx(hdc, 100, 100, &pt);
+		::LineTo(hdc, 100, 200);
+		::LineTo(hdc, 200, 200);
+		::LineTo(hdc, 200, 100);
 		::LineTo(hdc, 100, 100);
+
+		::Rectangle(hdc, 150, 150, 250, 250);
+
+		::Ellipse(hdc, 0,0,100,100);
+
+		::SelectObject(hdc, hOldBrush);
+		::DeleteObject(hBrush);
 
 		::SelectObject(hdc, hOldPen);
 		::DeleteObject(hPen);
