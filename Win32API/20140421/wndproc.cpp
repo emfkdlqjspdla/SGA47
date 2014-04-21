4638d662ed98720cc4faeafd5cf4682ab8cc3873
jklim <gocobe1228@naver.com>
2014-04-21 18:26:49 +0900

    @0421 project added.
    ..

---
+++ Win32API/20140421/wndproc.cpp
@@ -0,0 +1,49 @@
+﻿#include "windefine.h"
+
+LRESULT CALLBACK WndProc(HWND hWnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
+{
+	static BYTE vKey = 0x00;
+	static POINT ptMouse = {0,0};
+	static RECT rc = {0,0,0,0};
+
+	if (uMsg == WM_DESTROY)
+	{
+		::PostQuitMessage(0);
+		return 0;
+	}
+	else if (uMsg == WM_PAINT)
+	{
+		::GetClientRect(hWnd, &rc);
+
+		PAINTSTRUCT ps;
+		// Handle Device Context
+		HDC hdc = ::BeginPaint(hWnd, &ps);
+
+		TCHAR szBuffer[100];
+
+		_stprintf_s(szBuffer, _T("%x\r\nx = %d\r\ny = %d"), vKey, ptMouse.x, ptMouse.y);
+
+		::DrawText(hdc, szBuffer, -1, &rc, DT_TOP | DT_LEFT);
+
+		::EndPaint(hWnd, &ps);
+		return 0;
+	}
+	else if (uMsg == WM_KEYDOWN)
+	{
+		vKey = wParam;
+
+		::InvalidateRect(hWnd, &rc, TRUE);
+		return 0;
+	}
+	else if (uMsg == WM_LBUTTONDOWN)
+	{
+		ptMouse.x = GET_X_LPARAM(lParam);
+		ptMouse.y = GET_Y_LPARAM(lParam);
+
+		::InvalidateRect(hWnd, &rc, TRUE);
+
+		return 0;
+	}
+
+	return ::DefWindowProc(hWnd,uMsg,wParam,lParam);
+}
