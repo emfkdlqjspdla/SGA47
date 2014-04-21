96a3ee3a2993284da6162bb974eac1a84e41822f
jklim <gocobe1228@naver.com>
2014-04-21 19:11:38 +0900

    @0421 색 변화
    ..

--- Win32API/20140421/wndproc.cpp
+++ Win32API/20140421/wndproc.cpp
@@ -5,6 +5,12 @@ LRESULT CALLBACK WndProc(HWND hWnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
 	static BYTE vKey = 0x00;
 	static POINT ptMouse = {0,0};
 	static RECT rc = {0,0,0,0};
+	static int r = 15;
+	static COLORREF color = RGB(0,0,255);
+	static bool bRPlus = true;
+	static bool bGPlus = true;
+	static bool bBPlus = true;
+
 
 	if (uMsg == WM_DESTROY)
 	{
@@ -25,12 +31,10 @@ LRESULT CALLBACK WndProc(HWND hWnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
 
 		::DrawText(hdc, szBuffer, -1, &rc, DT_TOP | DT_LEFT);
 
-		const int r = 15;
-
-		HBRUSH hBrush = ::CreateSolidBrush(RGB(0,0,255));
+		HBRUSH hBrush = ::CreateSolidBrush(color);
 		HBRUSH hOldBrush = (HBRUSH)::SelectObject(hdc, hBrush);
 
-		HPEN hPen = ::CreatePen(PS_SOLID, 1, RGB(0,0,255));
+		HPEN hPen = ::CreatePen(PS_SOLID, 1, color);
 		HPEN hOldPen = (HPEN)::SelectObject(hdc, hPen);
 
 		::Ellipse(hdc, ptMouse.x-r,ptMouse.y-r,
@@ -49,7 +53,92 @@ LRESULT CALLBACK WndProc(HWND hWnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
 	{
 		vKey = wParam;
 
-		::InvalidateRect(hWnd, &rc, TRUE);
+		if (vKey == VK_ADD)
+		{
+			r++;
+			::InvalidateRect(hWnd, &rc, TRUE);
+		}
+		else if (vKey == VK_SUBTRACT)
+		{
+			r--;
+			::InvalidateRect(hWnd, &rc, TRUE);
+		}
+		else if (vKey == 'R')
+		{
+			if (GetRValue(color) == 255)
+			{
+				bRPlus = false;
+			}
+			else if (GetRValue(color) == 0)
+			{
+				bRPlus = true;
+			}
+
+			if (bRPlus)
+			{
+				color = RGB(GetRValue(color)+1,
+					GetGValue(color),
+					GetBValue(color));
+			}
+			else
+			{
+				color = RGB(GetRValue(color)-1,
+					GetGValue(color),
+					GetBValue(color));
+			}
+			::InvalidateRect(hWnd, &rc, TRUE);
+		}
+		else if (vKey == 'G')
+		{
+			if (GetGValue(color) == 255)
+			{
+				bGPlus = false;
+			}
+			else if (GetGValue(color) == 0)
+			{
+				bGPlus = true;
+			}
+
+			if (bGPlus)
+			{
+				color = RGB(GetRValue(color),
+					GetGValue(color)+1,
+					GetBValue(color));
+			}
+			else
+			{
+				color = RGB(GetRValue(color),
+					GetGValue(color)-1,
+					GetBValue(color));
+			}
+			::InvalidateRect(hWnd, &rc, TRUE);
+		}
+		else if (vKey == 'B')
+		{
+			if (GetBValue(color) == 255)
+			{
+				bBPlus = false;
+			}
+			else if (GetBValue(color) == 0)
+			{
+				bBPlus = true;
+			}
+
+			if (bBPlus)
+			{
+				color = RGB(GetRValue(color),
+					GetGValue(color),
+					GetBValue(color)+1);
+			}
+			else
+			{
+				color = RGB(GetRValue(color),
+					GetGValue(color),
+					GetBValue(color)-1);
+			}
+			::InvalidateRect(hWnd, &rc, TRUE);
+		}
+
 		return 0;
 	}
 	else if (uMsg == WM_MOUSEMOVE)
