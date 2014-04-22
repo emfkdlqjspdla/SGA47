263ebaa050d3fa53cb7b251c8275528f762befac
jklim <gocobe1228@naver.com>
2014-04-22 18:22:41 +0900

    @0422 프로젝트 추가.
    ..

---
+++ Win32API/20140422/windefine.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include <windows.h>
+#include <windowsx.h>
+#include <tchar.h>
+#include <stdlib.h> // rand, srand
+#include <time.h>
+#include <math.h>
+
+#define D2R 0.0174532f
+
+#include "InputDevice.h"
+
+LRESULT CALLBACK WndProc(HWND,UINT,WPARAM,LPARAM);
---
+++ Win32API/20140422/wndproc.cpp
@@ -0,0 +1,22 @@
+﻿#include "windefine.h"
+
+LRESULT CALLBACK WndProc(HWND hWnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
+{
+	if (uMsg == WM_DESTROY)
+	{
+		::PostQuitMessage(0);
+		return 0;
+	}
+	else if (uMsg == WM_PAINT)
+	{
+		PAINTSTRUCT ps;
+		// Handle Device Context
+		HDC hdc = ::BeginPaint(hWnd, &ps);
+
+
+		::EndPaint(hWnd, &ps);
+		return 0;
+	}
+
+	return ::DefWindowProc(hWnd,uMsg,wParam,lParam);
+}
