170e1d27efa83e479bbf8a5a32d10cd17ef11b6c
jklim <gocobe1228@naver.com>
2014-04-23 19:17:04 +0900

    @0423 프로젝트 추가
    ..

---
+++ Win32API/20140423/wndproc.cpp
@@ -0,0 +1,34 @@
+﻿#include "windefine.h"
+
+LRESULT CALLBACK WndProc(HWND hWnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
+{
+	static POINT ptCircle = {0,0};
+	static const LONG radius = 20;
+
+	if (uMsg == WM_CREATE)
+	{
+		return 0;
+	}
+	else if (uMsg == WM_DESTROY)
+	{
+		::PostQuitMessage(0);
+		return 0;
+	}
+	else if (uMsg == WM_PAINT)
+	{
+		RECT rc;
+		::GetClientRect(hWnd, &rc);
+
+		PAINTSTRUCT ps;
+		// Handle Device Context
+		HDC hdc = ::BeginPaint(hWnd, &ps);
+
+		::Ellipse(hdc, 
+
+
+		::EndPaint(hWnd, &ps);
+		return 0;
+	}
+
+	return ::DefWindowProc(hWnd,uMsg,wParam,lParam);
+}
