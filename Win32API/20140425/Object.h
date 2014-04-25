13ac41d7b57a100d4a897a3bb782db20d3e4227e
jklim <gocobe1228@naver.com>
2014-04-25 19:24:08 +0900

    0425 프로젝트 추가.
    ..

---
+++ Win32API/20140425/Object.h
@@ -0,0 +1,164 @@
+﻿#pragma once
+
+#include <math.h>
+#include <windows.h>
+
+class Object
+{
+public :
+	virtual void Update(DWORD tick) = 0;
+	virtual void Draw(HDC hdc) = 0;
+	POINT getCenter() const
+	{
+		return ptCenter;
+	}
+	LONG getRadius() const
+	{
+		return radius;
+	}
+	void SetCenter(const POINT& pt)
+	{
+		ptCenter.x = pt.x;
+		ptCenter.y = pt.y;
+	}
+
+protected :
+	POINT ptCenter;
+	LONG radius;
+};
+
+class Circle : public Object
+{
+public :
+	Circle(const SIZE& _area)
+		: area(_area)
+		, update_dt(0), delay(50)
+		, dx(5), xVel(5)
+	{
+		velocity = 0;
+
+		ptCenter.x = rand()%300 + 50;
+		ptCenter.y = rand()%100 + 50;
+
+		radius = rand()%50 + 10;
+
+		BYTE r = rand()%256;
+		BYTE g = rand()%256;
+		BYTE b = rand()%256;
+
+		color = RGB(r,g,b);
+	}
+	virtual void Update(DWORD tick)
+	{
+		if (update_dt >= delay)
+		{
+			int count = update_dt/delay;
+
+			for (int i = 0; i < count; i++)
+			{
+				velocity += 1;
+				ptCenter.y += velocity;
+
+				ptCenter.x += dx;
+
+				if (ptCenter.y + radius >= area.cy)
+				{
+					velocity = -velocity - 1;
+				}
+
+				if (ptCenter.x + radius >= area.cx)
+				{
+					dx = -xVel;
+				}
+				else if (ptCenter.x - radius <= 0)
+				{
+					dx = xVel;
+				}
+			}
+
+			update_dt %= delay;
+		}
+
+		update_dt += tick;
+	}
+	virtual void Draw(HDC hdc)
+	{
+		HPEN hPen = ::CreatePen(PS_SOLID, 1, color);
+		HPEN hOldPen = (HPEN)::SelectObject(hdc, hPen);
+
+		HBRUSH hBrush = ::CreateSolidBrush(color);
+		HBRUSH hOldBrush = (HBRUSH)::SelectObject(hdc, hBrush);
+
+		::Ellipse(hdc, 
+			ptCenter.x - radius,
+			ptCenter.y - radius,
+			ptCenter.x + radius,
+			ptCenter.y + radius);
+
+		::SelectObject(hdc, hOldBrush);
+		::DeleteObject(hBrush);
+
+		::SelectObject(hdc, hOldPen);
+		::DeleteObject(hPen);
+	}
+
+private :
+	LONG velocity;
+	COLORREF color;
+	SIZE area;
+	DWORD update_dt;
+	DWORD delay;
+	LONG dx;
+	LONG xVel;
+};
+
+class MouseCircle : public Object
+{
+public :
+	MouseCircle(HWND hWnd = NULL)
+		: hOwner(hWnd)
+	{
+		ptCenter.x = 0;
+		ptCenter.y = 0;
+
+		radius = 50;
+
+		color = RGB(255,0,0);
+	}
+	void Attach(HWND hWnd)
+	{
+		hOwner = hWnd;
+	}
+	void Update(DWORD tick)
+	{
+		// GetCursorPos 는 screen coord 값을 가져옴.
+		::GetCursorPos(&ptCenter);
+		// screen coord => client coord
+		::ScreenToClient(hOwner, &ptCenter);
+	}
+	void Draw(HDC hdc)
+	{
+		HPEN hPen = ::CreatePen(PS_SOLID, 1, color);
+		HPEN hOldPen = (HPEN)::SelectObject(hdc, hPen);
+
+		HBRUSH hBrush = ::CreateSolidBrush(color);
+		HBRUSH hOldBrush = (HBRUSH)::SelectObject(hdc, hBrush);
+
+		::Ellipse(hdc, 
+			ptCenter.x - radius,
+			ptCenter.y - radius,
+			ptCenter.x + radius,
+			ptCenter.y + radius);
+
+		::SelectObject(hdc, hOldBrush);
+		::DeleteObject(hBrush);
+
+		::SelectObject(hdc, hOldPen);
+		::DeleteObject(hPen);
+	}
+private :
+	COLORREF color;
+	HWND hOwner;
+};
+
+bool IsCollide(Object* lhs, Object* rhs);
---
+++ Win32API/20140425/wndproc.cpp
@@ -0,0 +1,115 @@
+﻿#include "windefine.h"
+
+LRESULT CALLBACK WndProc(HWND hWnd,UINT uMsg,WPARAM wParam,LPARAM lParam)
+{
+	static const int count = 10;
+	static Object* marble[count] = {0};
+	static Object* objMouse = NULL;
+	static DWORD dt = 1;
+	static DWORD st = ::GetTickCount();
+
+	if (uMsg == WM_CREATE)
+	{
+		RECT rc;
+		::GetClientRect(hWnd, &rc);
+		SIZE area = {rc.right-rc.left, rc.bottom-rc.top};
+
+		// 원을 만들기
+		for (int i = 0; i < count; i++)
+		{
+			marble[i] = new Circle(area);
+		}
+
+		objMouse = new MouseCircle(hWnd);
+
+		::SetTimer(hWnd, 0, 10, NULL);
+		return 0;
+	}
+	else if (uMsg == WM_DESTROY)
+	{
+		// 만든 원을 삭제..
+		for (int i = 0; i < count; i++)
+		{
+			delete marble[i];
+		}
+
+		delete objMouse;
+
+		::KillTimer(hWnd, 0);
+		::PostQuitMessage(0);
+		return 0;
+	}
+	else if (uMsg == WM_PAINT)
+	{
+		RECT rc;
+		::GetClientRect(hWnd, &rc);
+
+		PAINTSTRUCT ps;
+		// Handle Device Context
+		HDC hdc = ::BeginPaint(hWnd, &ps);
+
+		// 만든 원 그리기..
+		for (int i = 0; i < count; i++)
+		{
+			marble[i]->Draw(hdc);
+		}
+
+		objMouse->Draw(hdc);
+
+		::EndPaint(hWnd, &ps);
+		return 0;
+	}
+	else if (uMsg == WM_TIMER)
+	{
+
+
+		// 만든 원 업데이트.
+		for (int i = 0; i < count; i++)
+		{
+			// 충돌을 했다면...
+			//if (IsCollide(marble[i], objMouse))
+			//{
+			//	POINT pt = marble[i]->getCenter();
+			//	if (marble[i]->getCenter().x > objMouse->getCenter().x)
+			//	{
+			//		pt.x += objMouse->getRadius();
+			//	}
+			//	else
+			//	{
+			//		pt.x -= objMouse->getRadius();
+			//	}
+			//	marble[i]->SetCenter(pt);
+			//}
+
+			//for (int j = i+1; j < count; j++)
+			//{
+			//	if (IsCollide(marble[i], marble[j]))
+			//	{
+			//		POINT pt = marble[i]->getCenter();
+			//		if (marble[i]->getCenter().x > marble[j]->getCenter().x)
+			//		{
+			//			pt.x += marble[j]->getRadius();
+			//		}
+			//		else
+			//		{
+			//			pt.x -= marble[j]->getRadius();
+			//		}
+			//		marble[i]->SetCenter(pt);
+			//	}
+			//}
+
+			marble[i]->Update(dt);
+		}
+
+		objMouse->Update(dt);
+
+		dt = ::GetTickCount() - st;
+		st = ::GetTickCount();
+
+		RECT rc;
+		::GetClientRect(hWnd, &rc);
+		::InvalidateRect(hWnd, &rc, TRUE);
+	}
+
+	return ::DefWindowProc(hWnd,uMsg,wParam,lParam);
+}
