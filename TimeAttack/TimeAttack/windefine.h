a5ef14934acf4e06cdd00aa3cbc8a240cacfea95
jklim <gocobe1228@naver.com>
2014-04-25 19:18:26 +0900

    타임어택 코드 추가
    ...

---
+++ TimeAttack/TimeAttack/windefine.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include <windows.h>
+#include <windowsx.h>
+#include <tchar.h>
+
+LRESULT CALLBACK WndProc(HWND,UINT,WPARAM,LPARAM);
+
+
+/*
+20140423 : 0
+20140424 : 1
+20140425 : 0
+
+*/
---
+++ TimeAttack/TimeAttack/winmain.cpp
@@ -0,0 +1,58 @@
+﻿#include "windefine.h"
+
+INT WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE, LPTSTR, INT)
+{
+	LPCTSTR szClassName = _T("MyMainWindowClass");
+
+	WNDCLASSEX wcex = {0};
+
+    wcex.cbSize = sizeof(wcex);
+    wcex.style = CS_VREDRAW | CS_HREDRAW;
+    wcex.lpfnWndProc = &WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInst;
+	wcex.hIcon = ::LoadIcon(NULL, IDI_APPLICATION);
+	wcex.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)::GetStockObject(WHITE_BRUSH);
+    wcex.lpszMenuName = 0;
+    wcex.lpszClassName = szClassName;
+	wcex.hIconSm = wcex.hIcon;
+
+	if (!::RegisterClassEx(&wcex))
+	{
+		return 1;
+	}
+
+	int cx = ::GetSystemMetrics(SM_CXSCREEN);
+	int cy = ::GetSystemMetrics(SM_CYSCREEN);
+
+	int x = (cx - 400)/2;
+	int y = (cy - 400)/2;
+
+	DWORD dwStyle = WS_OVERLAPPEDWINDOW;
+	dwStyle = dwStyle & ~WS_MINIMIZEBOX;
+	dwStyle = dwStyle & ~WS_MAXIMIZEBOX;
+
+	HWND hWnd = ::CreateWindowEx(0, szClassName, _T("Win32"), dwStyle,
+		x, y, 400, 400,
+		NULL, NULL, hInst, NULL);
+
+	::ShowWindow(hWnd, SW_NORMAL);
+	::UpdateWindow(hWnd);
+
+
+	MSG msg;
+
+	while (true)
+	{
+		if(::PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			::TranslateMessage(&msg);
+			::DispatchMessage(&msg);
+		}
+		if (msg.message == WM_QUIT)
+			break;
+	}
+	return msg.wParam;
+}
