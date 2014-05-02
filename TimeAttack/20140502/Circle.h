dc5fb43a125ddc748915f5584dde39269d665aa6
jklim <gocobe1228@naver.com>
2014-05-02 19:18:57 +0900

    ..
    .

---
+++ TimeAttack/20140502/Circle.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+struct Circle
+{
+	POINT center;
+	LONG radius;
+	bool select;
+	int id;
+
+	Circle():radius(20){}
+	void Draw(HDC hdc)
+	{
+		HBRUSH hBrush = NULL;
+		HBRUSH hOldBrush = NULL;
+		HPEN hPen = NULL;
+		HPEN hOldPen = NULL;
+		if (select)
+		{
+			hBrush = ::CreateSolidBrush(RGB(255,0,0));
+			hOldBrush = (HBRUSH)::SelectObject(hdc, hBrush);
+
+			hPen = ::CreatePen(PS_SOLID, 1, RGB(255,200,200));
+			hOldPen = (HPEN)::SelectObject(hdc, hPen);
+		}
+
+		::Ellipse(hdc, center.x - radius, center.y - radius,
+			center.x + radius, center.y + radius);
+
+		if (select)
+		{
+			::SelectObject(hdc, hOldPen);
+			::DeleteObject(hPen);
+
+			::SelectObject(hdc, hOldBrush);
+			::DeleteObject(hBrush);
+		}
+	}
+};
