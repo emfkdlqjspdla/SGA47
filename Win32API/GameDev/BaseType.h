2b9e12fd4434a0c87bfbf37d1871eb61e7652c93
gocobe <gocobe1228@gmail.com>
2014-05-15 07:02:09 +0900

    GameDev lib 프로젝트 추가.
    게임 기본 틀 만들때 사용할 기능들을 모아서 GameDev.lib 로 만듬.
    앞으로 기능 추가는 계속 될 예정.

---
+++ Win32API/GameDev/BaseType.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include <windows.h>
+
+struct Point;
+struct Size;
+struct Rect;
+
+struct Point : public POINT
+{
+	Point(const LONG& _x = 0, const LONG& _y = 0);
+	Point ToScreen(HWND);
+	Point ToClient(HWND);
+	Point operator << (const Size& cs);
+	Point operator >> (const Size& cs);
+};
+
+// wrapper struct
+struct Size : public SIZE
+{
+	Size(const LONG& _cx = 0, const LONG& _cy = 0);
+	friend Size operator * (const LONG& v, const Size& obj);
+	friend Size operator * (const Size& obj, const LONG& v);
+	friend Size operator * (const float& v, const Size& obj);
+	friend Size operator * (const Size& obj, const float& v);
+};
+
+struct Rect : public RECT
+{
+	Rect(const LONG& l = 0, const LONG& t = 0, const LONG& r = 0, const LONG& b = 0);
+	Rect(const Point& lt, const Point& rb);
+	Rect(const Point& ct, const Size& cs);
+	Point lefttop() const;
+	Point rightbottom() const;
+	LONG width() const;
+	LONG height() const;
+	Point center() const;
+	Rect ToScreen(HWND);
+	Rect ToClient(HWND);
+
+	Rect Offset(const Size& diff) const;
+	Rect operator << (const Size& diff) const;
+	Rect operator >> (const Size& diff) const;
+};
---
+++ Win32API/GameDev/GameDev.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#include <windows.h>
+#include <tchar.h>
+
+#include "BaseType.h"
+#include "Utility.hpp"
+#include "InputDevice.h"
+#include "MainWindow.hpp"
