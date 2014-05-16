fe64510e093a2fba4e52982a19a6bfd2a20770ef
jklim <gocobe1228@naver.com>
2014-05-16 21:43:03 +0900

    BoxManager 를 추가함.
    Box 를 관리하는 클래스인 BoxManager 를 추가하였음.

---
+++ TimeAttack/20140516/BoxManager.cpp
@@ -0,0 +1,48 @@
+﻿#include "BoxManager.h"
+
+BoxManager::BoxManager()
+: hOwner(NULL)
+{}
+BoxManager::~BoxManager()
+{}
+
+void BoxManager::Attach(HWND hWnd)
+{
+	hOwner = hWnd;
+}
+void BoxManager::Input(DWORD tick)
+{
+	Rect rc;
+	::GetClientRect(hOwner, &rc);
+
+	for (Iter it = boxlist.begin(); it != boxlist.end(); it++)
+	{
+		Rect tmp;
+		if (::IntersectRect(&tmp, &rc, &((*it)->GetDrawRect())))
+		{
+			(*it)->Input(tick);
+		}
+	}
+}
+void BoxManager::Update(DWORD tick)
+{
+	for (Iter it = boxlist.begin(); it != boxlist.end(); it++)
+	{
+		(*it)->Input(tick);
+	}
+}
+void BoxManager::Draw(HDC hdc)
+{
+	Rect rc;
+	::GetClientRect(hOwner, &rc);
+
+	for (Iter it = boxlist.begin(); it != boxlist.end(); it++)
+	{
+		Rect tmp;
+		if (::IntersectRect(&tmp, &rc, &((*it)->GetDrawRect())))
+		{
+			(*it)->Draw(hdc);
+		}
+	}
+}
+
---
+++ TimeAttack/20140516/BoxManager.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <list>
+#include "Box.h"
+
+class BoxManager
+{
+	typedef std::list<Box*> List;
+	typedef List::iterator Iter;
+public :
+	BoxManager();
+	~BoxManager();
+
+	void Attach(HWND);
+	void Input(DWORD);
+	void Update(DWORD);
+	void Draw(HDC);
+
+private :
+	HWND hOwner;
+
+	List boxlist;
+};
