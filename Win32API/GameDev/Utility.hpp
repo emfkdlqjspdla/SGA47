1c65539959f74672a3edea47e1891aafe7784bc2
jklim <gocobe1228@naver.com>
2014-05-16 15:51:34 +0900

    SafeDelete, strAlloc 추가
    SafeDelete : 메모리 해제하는 함수
    strAlloc : 문자열을 메모리 할당해서 복사하는 함수.

--- Win32API/GameDev/Utility.hpp
+++ Win32API/GameDev/Utility.hpp
@@ -22,3 +22,28 @@ GDIObject Select(HDC hdc, GDIObject obj)
 {
 	return (GDIObject)::SelectObject(hdc, obj);
 }
+
+template<typename T>
+void SafeDelete(T& pointer)
+{
+	if (pointer)
+	{
+		delete [] pointer;
+		pointer = NULL;
+	}
+}
+
+template<typename CharType>
+CharType* strAlloc(CharType* & dest, const CharType* & src)
+{
+	SafeDelete(dest);
+
+	unsigned int i = 0;
+	while (src[i++]);
+
+	dest = new CharType[i];
+
+	while (dest[i++] = src[i]);
+
+	return dest;
+}
